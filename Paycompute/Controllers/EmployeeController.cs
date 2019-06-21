87ff972fb1b6d46c96967d3060a243aec9dad8fc
Everest Nwosu <everest@codewitheverest.com>
2019-06-21 15:04:23 +0100

    Index - EmployeeController

---
+++ Paycompute/Controllers/EmployeeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Paycompute.Models;
+using Paycompute.Services;
+
+namespace Paycompute.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            var employees = _employeeService.GetAll().Select(employee => new EmployeeIndexViewModel
+            {
+                Id = employee.Id,
+                EmployeeNo = employee.EmployeeNo,
+                ImageUrl = employee.ImageUrl,
+                FullName = employee.FullName,
+                Gender = employee.Gender,
+                Designation = employee.Designation,
+                City = employee.City,
+                DateJoined = employee.DateJoined
+            }).ToList();
+            return View(employees);
+        }
+    }
+}
---
+++ Paycompute/Models/EmployeeIndexViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paycompute.Models
+{
+    public class EmployeeIndexViewModel
+    {
+        public int Id { get; set; }
+        public string EmployeeNo { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime DateJoined { get; set; }
+        public string Designation { get; set; }
+        public string City { get; set; }
+
+    }
+}
