a6ff84673d1b00198f64705ee14e38a6075fdd60
Everest Nwosu <everest@codewitheverest.com>
2019-06-25 05:14:17 +0100

    taxService - Interface and Implementation

---
+++ Paycompute.Services/ITaxService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paycompute.Services
+{
+    public interface ITaxService
+    {
+        decimal TaxAmount(decimal totalAmount);
+    }
+}
---
+++ Paycompute.Services/Implementation/TaxService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Paycompute.Services.Implementation
+{
+    class TaxService
+    {
+    }
+}
